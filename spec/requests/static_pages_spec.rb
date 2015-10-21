be5a303c732d4ac88d5ad4267a8882afcdb4824a
zhengyang <zhengyangas@gmail.com>
2015-10-21 07:43:43 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
 
@@ -16,6 +21,12 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
 
 
@@ -25,6 +36,11 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+
   end
 
 
