dcbe20138580f5e452f4ee48fdac3e25f5f73b8b
zhengyang <zhengyangas@gmail.com>
2015-10-25 06:45:24 -0700

    Finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  #get "users/new"
+  resources :users
   match '/signup', to: 'users#new', via: 'get'
   #get "static_pages/home"
   #match '/root', to: 'static_pages#home', via: 'get'
