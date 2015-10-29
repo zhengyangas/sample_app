3304396b9dd9888cea1ff4a334848a24078c2533
zhengyang <zhengyangas@gmail.com>
2015-10-29 08:20:05 -0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,6 +7,12 @@ gem 'bootstrap-sass','2.3.2.0'
 
 gem 'bcrypt-ruby','3.0.1'
 
+gem "faker", '1.1.2'
+
+gem "will_paginate", "3.0.4"
+
+gem "bootstrap-will_paginate", '0.0.9'
+
 # Use sqlite3 as the database for Active Record
 group :development do
 	gem 'sqlite3','1.3.7'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index 
+    @users =User.paginate(page: params[:page])
+  end
+  
   def new
   	@user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   def create
@@ -14,13 +28,52 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
   	@user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
+
+
   def user_params
   	params.require(:user).permit(:name, :email, :password,
   								:password_confirmation)
   end
+
+  def signed_in_user
+    #redirect_to signin_url, notice: "please sign in" unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in"
+    end
+  end
+
+  def correct_user
+    @user =User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+      
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+      
+  end
+
+      
+
 end
